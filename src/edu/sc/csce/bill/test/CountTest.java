d281a9373878c4b983d86d1e7901226d512ae88e
Herbert <ahow0512@gmail.com>
2017-12-14 18:15:54 -0500

    add class

---
+++ src/edu/sc/csce/bill/test/CountTest.java
@@ -0,0 +1,16 @@
+package edu.sc.csce.bill.test;
+
+import org.junit.After;
+import org.junit.Before;
+
+public class CountTest {
+	
+	@Before
+	public void setUp() throws Exception {
+	}
+	
+	@After
+	public void tearDown() throws Exception {
+	}
+
+}
